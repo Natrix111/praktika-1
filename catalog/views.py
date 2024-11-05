5705f064885a39c085f9bb32d5be07b0b3559698
Natrix111 <dmitryk414@gmail.com>
2024-11-05 13:16:27 +0300

    add changes on main page

--- catalog/views.py
+++ catalog/views.py
@@ -6,10 +6,14 @@ def index(request):
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_authors=Author.objects.count()
+    num_genres = Genre.objects.count()
+
+    word = 'город'
+    num_books_with_word = Book.objects.filter(title__icontains=word).count()
 
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres': num_genres, 'num_books_with_word': num_books_with_word,},
     )
