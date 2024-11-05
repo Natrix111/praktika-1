8ddb84970feb5075064b2b1208d36382a56eaeb5
Natrix111 <dmitryk414@gmail.com>
2024-11-05 15:04:56 +0300

    session

--- catalog/views.py
+++ catalog/views.py
@@ -13,10 +13,13 @@ def index(request):
     word = 'город'
     num_books_with_word = Book.objects.filter(title__icontains=word).count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres': num_genres, 'num_books_with_word': num_books_with_word,},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres': num_genres, 'num_books_with_word': num_books_with_word, 'num_visits':num_visits},
     )
 
 class BookListView(generic.ListView):
